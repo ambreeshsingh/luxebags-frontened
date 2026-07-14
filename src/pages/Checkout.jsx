2f9be7f29177385d0b8e9a840f0468386edf04fb
Ambreesh Singh <ambreeshsingh2003@gmail.com>
2026-07-14 16:57:59 +0530

    clearCart fixed

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,6 +5,7 @@ import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 const { cartItems, totalPrice, clearCart } = useCart();
+const { name } = useAuth();
 
 export default function Checkout() {
   const { cartItems, totalPrice } = useCart();
